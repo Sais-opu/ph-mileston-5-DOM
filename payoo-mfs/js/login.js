c00696da5435a19d11fd25a797aaa01674c4a904
Sais-opu <saidulislamapu2024@gmail.com>
2024-09-21 21:19:24 +0600

    keno

---
+++ payoo-mfs/js/login.js
@@ -0,0 +1,22 @@
+// console.log('kemon aso')
+// search: form submit reloading the page
+
+// step-1 set event handler
+// document.getElementById('button-login').addEventListener('click', function (event) {
+//     // step-2 prevent default behavior(reloading browser)
+//     event.preventDefault(); // vejal to beginner
+//     console.log('Log in button click');
+
+//     //step-3 get the phone Number and the phone number
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber,pinNumber)
+//     // step-4 validate phone and pin
+//     if (phoneNumber === '5' && pinNumber === '1234') {
+//         console.log('You are loggin in')
+//         //step-5 allow user to use the website
+//     }
+//     else {
+//         alert('Wrong phone number or pin')
+//     }
+// })
