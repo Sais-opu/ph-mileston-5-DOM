a633a49f36e4796c93e06e32ac3a165be96d830d
Sais-opu <saidulislamapu2024@gmail.com>
2024-09-22 18:34:23 +0600

    ki holo

--- payoo-mfs-function/js/addMoney.js
+++ payoo-mfs-function/js/addMoney.js
@@ -6,12 +6,20 @@
  * 4. parseFloat
  *
  */
-document.getElementById('btn-add-money').addEventListener('click.', function (event) {
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
     event.preventDefault();
     // const addMoney = document.getElementById('input-add-money').value;
     // const addMoneyNumber= parseFloat(addMoney)
-    console.log('Add money button clicked');
-    getInputFieldById();
+    // console.log('Add money button clicked');
+    // getInputFieldValueById();
+    // const addMoney = getInputFieldValueById();
+    // console.log('add money value', addMoney);
+
+    const addMoney = getInputFieldValueById('input-add-money');
+    const pinNumber = getInputFieldValueById('input-pin-number');
+    console.log('add money with parameter', addMoney);
+    console.log('Pin number with parameter', pinNumber);
+
 
 
 });
--- payoo-mfs-function/js/utilities.js
+++ payoo-mfs-function/js/utilities.js
@@ -1,6 +1,12 @@
 /**
  * common shared function here
  */
-function getInputFieldById() {
-    console.log('will get value by id');
-};
+// function getInputFieldValueById() {
+//     // console.log('will get value by id');
+//     const addMoney = document.getElementById('input-add-money').value;
+//     return addMoney; // addMoney.js line-15
+// };
+function getInputFieldValueById(id) { // (id) as parametre for all id eta mane holo all id mann niye nibe ora
+    const inputValue = document.getElementById(id).value;
+    return inputValue; // addMoney.js line-18
+}
